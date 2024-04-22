57b2d71424954b636059313bbe54b55011bc79a2
winstonyang117 <winstonyang117@gmail.com>
2024-04-22 16:26:47 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def root():
+    return "Hello Flask"
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
